6e94d8954f336627d14a83cd9c51e63abe03e76a
eagletmt <eagletmt@gmail.com>
2013-03-25 17:09:33 +0900

    Improve test output

--- test.sh
+++ test.sh
@@ -10,9 +10,13 @@ for f in test/test_*.vim
 do
   testname=${f#test/test_}
   testname=${testname%.vim}
-  vim -e -N -u NONE -S test/before.vim -S "$f" -c quit
-  diff -u test/ok/$testname.ok test/output/$testname.out
-  retval=$[retval + $?]
+  if vim -e -N -u NONE -S test/before.vim -S "$f" -c quit < /dev/null; then
+    diff -u test/ok/$testname.ok test/output/$testname.out
+    retval=$[retval + $?]
+  else
+    echo "$testname: vim exited with $?"
+    retval=$[retval + 1]
+  fi
 done
 
 exit $retval
